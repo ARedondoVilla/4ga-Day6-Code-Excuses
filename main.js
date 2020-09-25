3192707104a91e18b98974dae23843532205118d
ARedondoVilla <alexredondovilla@gmail.com>
2020-09-25 19:07:50 +0000

    Finalizado

---
+++ main.js
@@ -0,0 +1,16 @@
+let who = ['The dog','My grandma','His turtle','My bird'];
+let action = ['ate','peed','crushed','broke'];
+let what = ['my homework', 'the keys', 'the car'];
+let when = ['before the class','right on time','when I finished','during my lunch','while I was praying'];
+
+let numRandom1 = Math.floor(Math.random() * 3);
+
+let numRandom2 = Math.floor(Math.random() * 4);
+
+let numRandom3 = Math.floor(Math.random() * 4);
+
+let numRandom4 = Math.floor(Math.random() * 5);
+
+console.log(who[numRandom2]+" "+action[numRandom3]+" "+what[numRandom1]+" "+when[numRandom4]);
+
+
