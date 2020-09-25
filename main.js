13fd4f8f00fee78331f02c5bfb351803dcaa4a3b
ARedondoVilla <alexredondovilla@gmail.com>
2020-09-25 19:10:42 +0000

    Finalizado revA

--- main.js
+++ main.js
@@ -3,14 +3,14 @@ let action = ['ate','peed','crushed','broke'];
 let what = ['my homework', 'the keys', 'the car'];
 let when = ['before the class','right on time','when I finished','during my lunch','while I was praying'];
 
-let numRandom1 = Math.floor(Math.random() * 3);
+let numRandom1 = Math.floor(Math.random() * 4);
 
 let numRandom2 = Math.floor(Math.random() * 4);
 
-let numRandom3 = Math.floor(Math.random() * 4);
+let numRandom3 = Math.floor(Math.random() * 3);
 
 let numRandom4 = Math.floor(Math.random() * 5);
 
-console.log(who[numRandom2]+" "+action[numRandom3]+" "+what[numRandom1]+" "+when[numRandom4]);
+console.log(who[numRandom1]+" "+action[numRandom2]+" "+what[numRandom3]+" "+when[numRandom4]);
 
 
