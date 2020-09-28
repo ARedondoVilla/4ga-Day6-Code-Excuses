3d789af546da4a949064efa59818d0a8e4a8f741
ARedondoVilla <alexredondovilla@gmail.com>
2020-09-28 14:52:29 +0000

    Finalizado revB

--- main.js
+++ main.js
@@ -1,16 +1,15 @@
-let who = ['The dog','My grandma','His turtle','My bird'];
-let action = ['ate','peed','crushed','broke'];
-let what = ['my homework', 'the keys', 'the car'];
-let when = ['before the class','right on time','when I finished','during my lunch','while I was praying'];
+window.onload = function() {    
+    let who = ['The dog','My grandma','His turtle','My bird'];
+    let action = ['ate','peed','crushed','broke'];
+    let what = ['my homework', 'the keys', 'the car'];
+    let when = ['before the class','right on time','when I finished','during my lunch','while I was praying'];
 
-let numRandom1 = Math.floor(Math.random() * 4);
+    let whoExcuse = who[Math.floor(Math.random() * who.length)];
+    let actionExcuse = action[Math.floor(Math.random() * action.length)];
+    let whatExcuse = what[Math.floor(Math.random() * what.length)];
+    let whenExcuse = when[Math.floor(Math.random() * when.length)];
 
-let numRandom2 = Math.floor(Math.random() * 4);
-
-let numRandom3 = Math.floor(Math.random() * 3);
-
-let numRandom4 = Math.floor(Math.random() * 5);
-
-console.log(who[numRandom1]+" "+action[numRandom2]+" "+what[numRandom3]+" "+when[numRandom4]);
-
-
+    let excuse = whoExcuse+" "+actionExcuse+" "+whatExcuse+" "+whenExcuse;
+    document.getElementById("generator").innerHTML = excuse;
+    // console.log(excuse);
+}
